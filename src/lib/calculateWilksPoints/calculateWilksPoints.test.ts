4690cad63c521262930deaa2d49ad6e6e9e85b67
Dimitar Banev <banev_dimitar@mail.com>
2024-01-05 11:39:19 +0200

    test(calculateWilksPoints): added cases for the new version

--- src/lib/calculateWilksPoints/calculateWilksPoints.test.ts
+++ src/lib/calculateWilksPoints/calculateWilksPoints.test.ts
@@ -21,3 +21,25 @@ describe("Testing original version", () => {
     expect(result).toBe(624.97);
   });
 });
+
+describe("Testing 2020 version", () => {
+  test("male and metric unit", () => {
+    const result = calculateWilksPoints("m", 89.9, 1022.5, "metric", true);
+    expect(result).toBe(784.7);
+  });
+
+  test("male and imperial unit", () => {
+    const result = calculateWilksPoints("m", 198.2, 2254.2, "imperial", true);
+    expect(result).toBe(784.7);
+  });
+
+  test("female and metric unit", () => {
+    const result = calculateWilksPoints("f", 84.4, 702.5, "metric", true);
+    expect(result).toBe(778.82);
+  });
+
+  test("female and imperial unit", () => {
+    const result = calculateWilksPoints("f", 186.1, 1548.7, "imperial", true);
+    expect(result).toBe(778.82);
+  });
+});
