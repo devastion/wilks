135392265f047ecd3489ad11dac4e904815ee10f
Dimitar Banev <banev_dimitar@mail.com>
2025-07-18 02:04:49 +0300

    Add wilks input validator

--- src/wilks/helpers.ts
+++ src/wilks/helpers.ts
@@ -1,7 +1,7 @@
 import * as constants from "./constants.ts";
 import { lbsToKilos } from "../utils.ts";
 
-import type { Lifter } from "./types.ts";
+import type { Lifter, WilksInput } from "./types.ts";
 
 export function calculateWilksCoefficient(input: Lifter, use2020Formula = false): number {
   const { gender, bodyweight, unit } = input;
@@ -17,4 +17,40 @@ export function calculateWilksCoefficient(input: Lifter, use2020Formula = false)
       ? acc + curr
       : acc + (curr * Math.pow(bw, idx));
   }, 0);
+}
+
+export function validateWilksInput({ bodyweight, gender, unit, total, squat, bench, deadlift }: WilksInput) {
+  if (typeof bodyweight !== "number" || bodyweight <= 0) {
+    throw new Error("Bodyweight must be number greater than 0");
+  }
+
+  if (!["m", "f"].includes(gender)) {
+    throw new Error("Gender must be either 'm' or 'f'");
+  }
+
+  if (!["kg", "lb"].includes(unit)) {
+    throw new Error("Unit must be either 'kg' or 'lb'");
+  }
+
+  if (total && (squat || bench || deadlift)) {
+    throw new Error("Use total or squat/bench/deadlift (not both)");
+  }
+
+  if (total && (typeof total !== "number" || total < 0)) {
+    throw new Error("Invalid total. Use number greater than 0");
+  }
+
+  if (!total) {
+    if (typeof squat !== "number" || squat < 0) {
+      throw new Error("Invalid squat. Use number greater than 0");
+    }
+
+    if (typeof bench !== "number" || bench < 0) {
+      throw new Error("Invalid bench. Use number greater than 0");
+    }
+
+    if (typeof deadlift !== "number" || deadlift < 0) {
+      throw new Error("Invalid deadlift. Use number greater than 0");
+    }
+  }
 }
--- src/wilks/index.ts
+++ src/wilks/index.ts
@@ -1,13 +1,15 @@
-import { calculateWilksCoefficient } from "./helpers.ts";
+import { calculateWilksCoefficient, validateWilksInput } from "./helpers.ts";
 import { lbsToKilos, roundToDecimal } from "../utils.ts";
 
 import type { WilksInput } from "./types.ts";
 
 export function wilks(input: WilksInput, use2020Formula = false): number {
-  const { gender, bodyweight, unit, ...rest } = input;
+  validateWilksInput(input);
+
+  const { gender, bodyweight, unit, total, ...rest } = input;
   const coefficient = calculateWilksCoefficient({ gender, bodyweight, unit }, use2020Formula);
-  const total = Object.values(rest).reduce((a, b) => a + b, 0);
-  const totalInKgs = unit === "lb" ? lbsToKilos(total) : total;
+  const totalSum = total ?? Object.values(rest).reduce((a, b) => a + b, 0);
+  const totalInKgs = unit === "lb" ? lbsToKilos(totalSum) : totalSum;
 
   const wilksScore = totalInKgs * ((use2020Formula ? 600 : 500) / coefficient);
 
