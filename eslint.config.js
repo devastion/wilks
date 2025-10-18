5cbde9d8f1d9ea1a32558a3f91b017648fd9281b
Dimitar Banev <banev_dimitar@mail.com>
2025-10-18 09:34:56 +0300

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
+import { defineConfig } from "eslint/config";
 import importPlugin from "eslint-plugin-import";
 import sonarjs from "eslint-plugin-sonarjs";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
